38424d24550a6c6f8f0259f05137ed424a868ed3
Nitin Chaurasia <gs.nitin@gmail.com>
2020-01-27 10:05:52 -0500

    Eureka naming server added

--- src/main/java/com/nitin/microservices/learning/currencyConversionService/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/nitin/microservices/learning/currencyConversionService/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,9 +2,11 @@ package com.nitin.microservices.learning.currencyConversionService.currencyconve
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients("com.nitin.microservices.learning.currencyConversionService.currencyconversionservice")
 public class CurrencyConversionServiceApplication {
 
