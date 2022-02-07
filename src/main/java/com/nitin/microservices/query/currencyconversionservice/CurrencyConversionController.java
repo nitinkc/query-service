65cd777227669fe170fe59e9a32740d3adc15559
Nitin <gs.nitin@gmail.com>
2022-02-06 21:50:05 -0600

    updates

--- src/main/java/com/nitin/microservices/query/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/nitin/microservices/query/currencyconversionservice/CurrencyConversionController.java
@@ -66,7 +66,7 @@ public class CurrencyConversionController {
 
     @GetMapping("/")
     public String welcomeMessage(){
-        String msg = "Hello Command";
+        String msg = "Hello Query";
         return msg;
     }
 }
--- src/main/java/com/nitin/microservices/query/currencyconversionservice/CurrencyExchangeServiceProxy.java
+++ src/main/java/com/nitin/microservices/query/currencyconversionservice/CurrencyExchangeServiceProxy.java
@@ -11,10 +11,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 //@FeignClient(name="currency-exchange-service",url = "localhost:8000")
 //@FeignClient(name="currency-exchange-service")
 @FeignClient(name="netflix-zuul-api-gateway-server")
-@RibbonClient(name="currency-exchange-service")
+@RibbonClient(name="command-service")
 public interface CurrencyExchangeServiceProxy {
 
     //@GetMapping("/foreign-exchange/from/{from}/to/{to}")
-    @GetMapping("currency-exchange-service/foreign-exchange/from/{from}/to/{to}")
+    @GetMapping("command-service/foreign-exchange/from/{from}/to/{to}")
     public CurrencyConversionBean retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
     }
